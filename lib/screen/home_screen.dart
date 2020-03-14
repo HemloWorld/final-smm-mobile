35e5e8cb79fc3c7f13520f9a1dda569bd5b11f92
Ahmad Ruslan <aruslan8395@gmail.com>
2020-03-14 14:02:13 +0700

    [EDIT] Cut the navigation bottom bar into a spliting widget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,54 +1,11 @@
+import 'package:final_project_mobile_app/widget/bottomnavbar_home.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
-
-  final tabs = [
-    Center(child: Text('Home')),
-    Center(child: Text('Absen')),
-    Center(child: Text('Profile'))
-  ];
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.red,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: Colors.white),
-            title: Text('Home'),
-            backgroundColor: Colors.red
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.camera_enhance,
-              color: Colors.white,),
-            title: Text('Absen'),
-            backgroundColor: Colors.red
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-              color: Colors.white,),
-            title: Text('Profile'),
-            backgroundColor: Colors.red
-          )
-        ],
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        } ,
-      ),
+      bottomNavigationBar: HomeBNB(),
     );
   }
 }
