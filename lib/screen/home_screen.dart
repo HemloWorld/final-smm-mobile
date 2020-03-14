49626a17f5c55ae0486b115bae9c20baaa84e9cf
Ahmad Ruslan <aruslan8395@gmail.com>
2020-03-14 17:06:15 +0700

    [TEST] Test the body properties

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'test',
+              style: TextStyle(
+                color: Colors.black
+              ),
+            )
+          ],
+        ),
+      ),
       bottomNavigationBar: HomeBNB(),
     );
   }
