b119463db874246c5b5f30010f1b2282921d476b
Ahmad Ruslan <aruslan8395@gmail.com>
2020-03-23 11:01:12 +0700

    [ADD] Add dividerText custom widget into login_widget

---
+++ lib/widgets/login_widget/divider_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DividerText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      ' - OR - ',
+      style: TextStyle(
+        color: Colors.black54,
+        fontWeight: FontWeight.w400
+      )
+    );
+  }
+}
+
+
