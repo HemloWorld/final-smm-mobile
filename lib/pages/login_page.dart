042a78132a8db1e302da3108b8cce52be124aa35
Ahmad Ruslan <aruslan8395@gmail.com>
2020-03-23 11:21:13 +0700

    [REFACTOR] Cleaning the code

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,4 @@
-import 'package:final_project/widgets/login_widget/divider_text.dart';
-import 'package:final_project/widgets/login_widget/email_field.dart';
-import 'package:final_project/widgets/login_widget/google_login_button.dart';
-import 'package:final_project/widgets/login_widget/login_button.dart';
-import 'package:final_project/widgets/login_widget/password_field.dart';
+import 'package:final_project/widgets/login_widget/login_master.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,27 +11,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 40),
-        decoration: BoxDecoration(color: Colors.white),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 20.0,),
-            Image.asset('assets/logo.png'),
-            SizedBox(height: 80.0),
-            EmailField(),
-            SizedBox(height: 20.0,),
-            PasswordField(),
-            SizedBox(height: 10.0,),
-            LoginButton(),
-            DividerText(),
-            GoogleLoginButton(),
-          ],
-        ),
-      )
-    );
+    return LoginMaster();
   }
 }
